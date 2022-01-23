bb312947f8b8afe5723d811feda3e201c1684848
Matthew Buglass <60230689+matthew-buglass@users.noreply.github.com>
2022-01-23 13:16:10 -0600

    Added comments to random_graph_generator

--- random_graph_generation.py
+++ random_graph_generation.py
@@ -4,10 +4,17 @@ import collections
 from data_structures.vertex_graph import VertexGraph
 from data_structures.vertex_point import VertexPoint
 
+"""
+Generates a graph for testing purposes
+"""
 
 def print_order(order: int, network):
     """
+    Prints out the graph in the order provided
+    
     :param order: 0 = in order; 1 = pre order; 2 = post order
+    :param network: the node network in a 3D-tree
+    
     :return:
     """
     print(network.to_string_coordinates(order))
@@ -22,13 +29,14 @@ def print_order(order: int, network):
     print("Percent adjacent: {:.5f}%".format(order_adjacent.count(True) / len(order_adjacent) * 100))
 
 
+# Generating the graph
 width = 10
 height = 60
 abs_coefficient = 0.005
 
 verts = []
 
-# making a hyperbolic paraboloid
+# making a hyperbolic paraboloid Vertex's
 for x in range(-width, width+1):
     for y in range(-height, height+1):
         z = abs_coefficient * (x**2) + -abs_coefficient * (y**2)
@@ -36,8 +44,10 @@ for x in range(-width, width+1):
         vert.set_coordinates(x, y, z)
         verts.append(vert)
 
+# Generating the network from the list of vericies    
 node_network = VertexGraph.from_list(verts)
 
+# Output the verticies that are within 1 meter of eachother
 print("Adjacent coordinates:")
 for i in range(-width, width+1):
     for j in range(-height, height+1):
@@ -45,7 +55,7 @@ for i in range(-width, width+1):
         adjs_co = [g.get_coordinates() for g in adjs]
         print("vertex ({}, {}, z). Within 1 meter x, y: {}".format(i, j, [(a[0], a[1]) for a in adjs_co]))
 
-
+# create edges between verticies that are withing 1 meter of eachother across the x and y co-ordinates
 print("\nEstablishing x_y proximity within 1 meter")
 node_network.establish_euclidean_proximity_xy(1)
 for vertex in node_network.get_vertices():
@@ -55,20 +65,24 @@ for vertex in node_network.get_vertices():
     print("vertex {} personal and graph adjacents identical: {}".format(vertex.get_coordinates(),
                                                                         collections.Counter(network_adj) == collections.Counter(vertex_adj)))
 
+# pretty print the verticies
 print("\nVertex to pretty json:")
 for vertex in node_network.get_vertices():
     print(vertex.to_pretty_json())
 
+# Pretty print graph
 print("\nGraph to pretty json:")
 graph_json = node_network.to_pretty_json()
 print(graph_json)
 new_graph = VertexGraph.from_json(graph_json)
 print(new_graph)
 
+# write the graph to file
 with open("jsons/test_data.json", "w+") as f:
     f.write(graph_json)
     f.close()
 
+# get and print the data required to build the graph as a mesh in Blender (https://www.blender.org/)
 vertices, edges, faces = node_network.get_blender_data()
 
 print("\nVertices:")
