94a4baeca5942c73d2e24e830e0d1f33b46c24e0
Matthew Buglass <60230689+matthew-buglass@users.noreply.github.com>
2022-01-23 13:25:59 -0600

    Commented and changed name of blender_objec...

--- blender_only/blender_obj_generator.py
+++ blender_only/blender_obj_generator.py
@@ -6,9 +6,16 @@ import sys
 from importlib.machinery import SourceFileLoader
 import time
 
-# imports the module from the given path
+"""
+Script to build a mesh from a vertex graph.
+
+CAN ONLY BE RUN FROM THE SCRIPTING TERMINAL IN THE BLENDER APPLICATON
+"""
+
+# imports the module containing the VertexGraph data structure from the given absolute path
 vg = SourceFileLoader("vertex_graph", "D:\\Bathymetric Mapping\\bathymetric_mapping_scripts\\data_structures\\vertex_graph.py").load_module()
 
+# load the graph from the given absolute path
 print(os.getcwd())
 file_name = "D:\\Bathymetric Mapping\\bathymetric_mapping_scripts\\jsons\\test_data.json"
 
@@ -18,16 +25,17 @@ with open(file_name, "r") as f:
 
 #print(data)
 
-# remaking the graph and establishing the point proximity
+# deserializing the graph and building the edges between adjacent verticies
 g = vg.VertexGraph.from_json(json.dumps(data))
 g.establish_euclidean_proximity_xy(1)
 vertices, edges, faces = g.get_blender_data()
 
+# instantiate a new mesh and define it's verticies, edges, and faces
 new_mesh=bpy.data.meshes.new("new_mesh")
 new_mesh.from_pydata(vertices, edges, faces)
 new_mesh.update()
 
-#make object from the mesh
+# make object from the mesh data
 new_object = bpy.data.objects.new("hyperbolic_paraboloid", new_mesh)
 
 view_layer=bpy.context.view_layer
